23480cf9d576d497be0d610458994ced18b005d5
Oumou2002 <os5173167@gmail.com>
2025-08-21 22:32:48 +0200

    Initial commit – projet MySkillHub

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -172,4 +172,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
-CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite dev server
+]
